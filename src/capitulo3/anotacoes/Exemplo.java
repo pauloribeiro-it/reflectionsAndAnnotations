fdb3b789907375b6db6a95a10c85241433596fbf
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-03-23 22:32:22 -0300

    Criação do segundo atributo da interface Metadado

---
+++ src/capitulo3/anotacoes/Exemplo.java
@@ -0,0 +1,12 @@
+package capitulo3.anotacoes;
+
+public class Exemplo {
+	@Metadado(0)
+	private String atributo1;
+	
+	@Metadado(value=1,tipo="OK")
+	private String atributo2;
+	
+	@Metadado(value=2,tipo="ok")
+	private String atributo3;
+}
--- src/capitulo3/anotacoes/Metadado.java
+++ src/capitulo3/anotacoes/Metadado.java
@@ -8,5 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD,ElementType.PACKAGE})
 public @interface Metadado {
-
+	int value();
+	String tipo();
 }
